8a96b6c451adf38aa988576ab7b3fbce9006cfd3
jesgodoy <jesicagodoy7@gmail.com>
2024-01-13 02:19:42 -0300

    feature/PreEntrega1 - correciones

--- js/micodigo.js
+++ js/micodigo.js
@@ -1,5 +1,5 @@
 let saludo = prompt(`Hola bienvenido a "DELICIAS" ¿Como es tu nombe?`)
-alert ("Hola "+saludo+" estoy aui para ayudarte a realizar tu compra")
+alert ("Hola "+saludo+" estoy aqui para ayudarte a realizar tu compra")
 
 // defino variables de los precios de los productos
 const vainilla = 175.50
@@ -12,9 +12,8 @@ const compraTotal= (producto, cantidad) => {return parseFloat(producto)* parseIn
 
 let ayudaCompra = true
 
-do{
+do{ // comiezo el bcle para compra
     let elegirProducto = parseInt(prompt("elegi el sabor de Cupcakes preferido: 1) Vainilla, 2) Coco, 3)Limon, 4)Chocolate 5)otro producto"))
-    var userInput = prompt("Ingrese algo (presione Cancelar para detener)");
     if(elegirProducto == 1 || elegirProducto == 2 || elegirProducto ==3 || elegirProducto == 4){
         let cantidad = parseInt(prompt("Ingresa aqui la cantidad de cupcakes que quieres Comprar"))
         switch (parseFloat(elegirProducto)){
@@ -37,16 +36,16 @@ do{
         }
     }else if (elegirProducto == 5){
         let presupuesto = parseInt(prompt("¿ cuanto dinero tenes para pagar?"))
-        if(presupuesto >= 175.5 && presupuesto < 1500 ){
+        if(presupuesto >= 175.5 && presupuesto < 5000 ){
             alert("tu presupuesto para compra esta un poco limitado, lo unico que puedo ofrecerte son cupcakes")
             break
-        }else if(presupuesto >= 1500 && presupuesto < 2600){
+        }else if(presupuesto >= 5000 && presupuesto < 6500){
             alert ("puedo ofrecerte una tota de vainilla")
             break
-        }else if(presupuesto>=2600 && presupuesto < 3500){
+        }else if(presupuesto >= 6500 && presupuesto < 3500){
             alert ("puedo ofrecerte una tota de vainilla o una tarta de frutilla")
             break
-        }if(presupuesto >= 3500 && presupuesto){
+        }if(presupuesto >= 3500 ){
             alert ("puedo ofrecerte una tota de vainilla, una tarta de frutilla o una chocotorta")
             break
         }else{
