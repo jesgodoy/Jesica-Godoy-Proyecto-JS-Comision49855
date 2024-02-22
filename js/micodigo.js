f0b21ea2d3fba722e43326f7ef52f0a2d7c20ac4
jesgodoy <jesicagodoy7@gmail.com>
2024-02-22 16:33:07 -0300

    feature/EntregaFinal cargade productos y filtro

--- js/micodigo.js
+++ js/micodigo.js
@@ -1,16 +1,83 @@
 const contenedorProductos = document.getElementById("contenedor-productos")
+const botonesFiltro = document.querySelectorAll(".botones-filtro")
 
-productos.forEach((producto)=>{
+
+
+function cargarProducto(filtro){
     
+    contenedorProductos.innerHTML="",
+    filtro.forEach((producto)=>{
     
     let contenidoProducto = document.createElement("div")
     contenidoProducto.innerHTML=`
-    <img src="${producto.img}" class="card-img-top" >
-    <h3 class="card-title">${producto.nombre}</h3>
-    <h3 class="card-text">$${producto.precio}</h3>
-    <a href="#" class="btn btn-primary">Comprar</a>
+    
+    <div class="card">
+        <div class="img-body container pt-3">
+        <img src="${producto.img}" class="card-img-top">
+        </div>
+        <div class="card-body">
+            <h4 class="card-title text-center">${producto.nombre}</h4>
+            <h4 class="card-text text-center">$${producto.precio}</h4>
+            <div class="text-center">
+            <button type="button" class="btn btn-primary" id="boton-comprar" >Comprar</button>
+            </div>
         </div>
     </div>
     `
     contenedorProductos.appendChild(contenidoProducto)
 })
+}
+cargarProducto(productosFebrero);
+
+
+botonesFiltro.forEach(boton =>{
+    boton.addEventListener("click", (e)=>{
+
+        botonesFiltro.forEach(boton=>boton.classList.remove("active"))
+        e.currentTarget.classList.add("active")
+        if(e.currentTarget.id !="productos"){
+            const botonesCategorias= productosFebrero.filter(producto=> producto.categoria === e.currentTarget.id)
+        cargarProducto(botonesCategorias);
+        }else{
+            cargarProducto(productosFebrero)
+        }
+        
+    } )
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*productosFebrero.forEach((producto)=>{
+    
+    
+    let contenidoProducto = document.createElement("div")
+    contenidoProducto.innerHTML=`
+    
+    <div class="card">
+        <div class="img-body container pt-3">
+        <img src="${producto.img}" class="card-img-top">
+        </div>
+        <div class="card-body">
+            <h4 class="card-title text-center">${producto.nombre}</h4>
+            <h4 class="card-text text-center">$${producto.precio}</h4>
+            <div class="text-center">
+            <button type="button" class="btn btn-primary">Comprar</button>
+            </div>
+        </div>
+    </div>
+    `
+    contenedorProductos.appendChild(contenidoProducto)
+})*/
--- js/productos.js
+++ js/productos.js
@@ -3,43 +3,49 @@ const productos = [
         id: 1,
         nombre: "Brownie Clasico", 
         precio: 10000,
-        img: "./assets/imagenes/productos/brownie-clasico.jpg",
-        cantidad: 1
+        img: "../assets/imagenes/productos/brownie-clasico.jpg",
+        cantidad: 1,
+        categoria: "brownie",
     },
     {
         id: 2,
         nombre: "Brownie Frambuesa", 
         precio: 14000,
         img: "../assets/imagenes/productos/brownie-frutilla.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "brownie",
     },
     {
         id: 3,
         nombre: "Brownie Mix Frutos ",      
         precio: 15000,
         img: "../assets/imagenes/productos/brownie-frutosrojos.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "brownie",
     },
     {
         id: 4,
         nombre: "Brownie Moka", 
         precio: 11000,
         img: "../assets/imagenes/productos/brownie-moka.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "brownie",
     },
     {
         id: 5,
         nombre: "Brownie Oreo", 
         precio: 15000,
         img: "../assets/imagenes/productos/brownie-oreo.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "brownie",
     },
     {
         id: 6,
         nombre: "Chocooreo", 
         precio: 17000,
         img: "../assets/imagenes/productos/chocoreo.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "torta",
     },
         
     {
@@ -47,77 +53,87 @@ const productos = [
         nombre: "Chocotorta", 
         precio: 18300,
         img: "../assets/imagenes/productos/choco-torta.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "torta",
     },
     {
         id: 8,
         nombre: "Lemon Pie", 
         precio: 10800,
         img: "../assets/imagenes/productos/lemon-pie.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "tartas",
     },
     {
         id: 9,
         nombre: "Tiramisú", 
         precio: 9000,
         img: "../assets/imagenes/productos/tiramisu.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "tartas",
     },
     {
         id: 10,
         nombre: "Rogel", 
         precio: 8000,
         img: "../assets/imagenes/productos/rogel.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "tartas",
     },
     {
         id: 11,
         nombre: "Tarta Frutos del bosque", 
         precio: 10000,
         img: "../assets/imagenes/productos/tarta-frutos-rojos.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "tartas",
     },
     {
         id: 12,
         nombre: "Cupcakes", 
         precio: 510,
         img: "../assets/imagenes/productos/cupcakes.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "bocaditos y postres",
     },
     {
         id: 13,
         nombre:"Shot Dulce", 
         precio: 1210,
         img: "../assets/imagenes/productos/shot-dulce.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "bocaditos y postres",
     },
     {
         id: 14,
         nombre:"Torta de Vainilla", 
         precio: 8510,
         img: "../assets/imagenes/productos/torta-vainilla.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "torta",
     },
     {
         id: 15,
         nombre:"Torta de Chocolate", 
         precio: 10600,
         img: "../assets/imagenes/productos/torta-chocolate.jpg",
-        cantidad: 1
+        cantidad: 1,
+        categoria: "torta",
     },
 ];
 
 //creo objetos atravez de una funcion constructorasa
-const SanValentin =  function(id, nombre, precio, img, cantidad){
+const SanValentin =  function(id, nombre, precio, img, cantidad, categoria){
     this.id = id
     this.nombre = nombre
     this.precio= precio
     this.img = img
+    this.categoria = categoria
 };
-let sanValentin1 = new SanValentin (16, "Box Love", 15000 ,"../assets/imagenes/productos/box-sanvalentin.jpg", 1);
-let sanValentin2 = new SanValentin (17, "Brownie LOVE", 21200, "../assets/imagenes/productos/brownie-love.jpg", 1);
-let sanValentin3 = new SanValentin (18, "Cupcakes de Amor", 700, "../assets/imagenes/productos/cupcakes-love.jpg", 1);
-let sanValentin4 = new SanValentin (19, "Torta Corazón", 15500, "../assets/imagenes/productos/torta-corazon.jpg", 1);
+let sanValentin1 = new SanValentin (16, "Box Love", 15000 ,"../assets/imagenes/productos/box-sanvalentin.jpg", 1, "bocaditos y postres" );
+let sanValentin2 = new SanValentin (17, "Brownie LOVE", 21200, "../assets/imagenes/productos/brownie-love.jpg", 1, "brownie");
+let sanValentin3 = new SanValentin (18, "Cupcakes de Amor", 700, "../assets/imagenes/productos/cupcakes-love.jpg", 1, "bocaditos y postres");
+let sanValentin4 = new SanValentin (19, "Torta Corazón", 15500, "../assets/imagenes/productos/torta-corazon.jpg", 1, "torta");
 
 //convierto en array los objeros creados
 let  productoSanValentin = [sanValentin1, sanValentin2, sanValentin3, sanValentin4];
@@ -135,6 +151,8 @@ const productosFebrero = listaProductos.map(producto =>{
             nombre: producto.nombre,
             precio:  Math.round(producto.precio *(1- descuento)),
             img: producto.img,
+            cantidad: producto.cantidad,
+            categoria: producto.categoria
         }
     }else{
         return producto
