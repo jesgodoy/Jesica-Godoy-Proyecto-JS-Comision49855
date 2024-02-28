b980f5078ec4660f5aa848e9a184280eebe862f7
jesgodoy <jesicagodoy7@gmail.com>
2024-02-27 21:08:52 -0300

    feature/EntregaFinal correcion de imagenes

--- js/productos.js
+++ js/productos.js
@@ -48,7 +48,7 @@ const productos = [
         id: 6,
         nombre: "Chocooreo", 
         precio: 17000,
-        img: "../assets/imagenes/productos/chocoreo.jpg",
+        img: "./assets/imagenes/productos/chocoreo.jpg",
         cantidad: 1,
         categoria: "torta",
         seccion: "general"
@@ -58,7 +58,7 @@ const productos = [
         id: 7,
         nombre: "Chocotorta", 
         precio: 18300,
-        img: "../assets/imagenes/productos/choco-torta.jpg",
+        img: "./assets/imagenes/productos/choco-torta.jpg",
         cantidad: 1,
         categoria: "torta",
         seccion: "general"
@@ -67,7 +67,7 @@ const productos = [
         id: 8,
         nombre: "Lemon Pie", 
         precio: 10800,
-        img: "../assets/imagenes/productos/lemon-pie.jpg",
+        img: "./assets/imagenes/productos/lemon-pie.jpg",
         cantidad: 1,
         categoria: "tartas",
         seccion: "general"
@@ -76,7 +76,7 @@ const productos = [
         id: 9,
         nombre: "Tiramisú", 
         precio: 9000,
-        img: "../assets/imagenes/productos/tiramisu.jpg",
+        img: "./assets/imagenes/productos/tiramisu.jpg",
         cantidad: 1,
         categoria: "tartas",
         seccion: "general"
@@ -85,7 +85,7 @@ const productos = [
         id: 10,
         nombre: "Rogel", 
         precio: 8000,
-        img: "../assets/imagenes/productos/rogel.jpg",
+        img: "./assets/imagenes/productos/rogel.jpg",
         cantidad: 1,
         categoria: "tartas",
         seccion: "general"
@@ -94,7 +94,7 @@ const productos = [
         id: 11,
         nombre: "Tarta Frutos del bosque", 
         precio: 10000,
-        img: "../assets/imagenes/productos/tarta-frutos-rojos.jpg",
+        img: "./assets/imagenes/productos/tarta-frutos-rojos.jpg",
         cantidad: 1,
         categoria: "tartas",
         seccion: "general"
@@ -103,7 +103,7 @@ const productos = [
         id: 12,
         nombre: "Cupcakes", 
         precio: 510,
-        img: "../assets/imagenes/productos/cupcakes.jpg",
+        img: "./assets/imagenes/productos/cupcakes.jpg",
         cantidad: 1,
         categoria: "bocaditos y postres",
         seccion: "general"
@@ -112,7 +112,7 @@ const productos = [
         id: 13,
         nombre:"Shot Dulce", 
         precio: 1210,
-        img: "../assets/imagenes/productos/shot-dulce.jpg",
+        img: "./assets/imagenes/productos/shot-dulce.jpg",
         cantidad: 1,
         categoria: "bocaditos y postres",
         seccion: "general"
@@ -121,7 +121,7 @@ const productos = [
         id: 14,
         nombre:"Torta de Vainilla", 
         precio: 8510,
-        img: "../assets/imagenes/productos/torta-vainilla.jpg",
+        img: "./assets/imagenes/productos/torta-vainilla.jpg",
         cantidad: 1,
         categoria: "torta",
         seccion: "general"
@@ -130,7 +130,7 @@ const productos = [
         id: 15,
         nombre:"Torta de Chocolate", 
         precio: 10600,
-        img: "../assets/imagenes/productos/torta-chocolate.jpg",
+        img: "./assets/imagenes/productos/torta-chocolate.jpg",
         cantidad: 1,
         categoria: "torta",
         seccion: "general"
@@ -147,10 +147,10 @@ const SanValentin =  function(id, nombre, precio, img, cantidad, categoria, secc
     this.categoria = categoria
     this.seccion = seccion
 };
-let sanValentin1 = new SanValentin (16, "Box Love", 15000 ,"../assets/imagenes/productos/box-sanvalentin.jpg", 1, "bocaditos y postres", "sanvalentin" );
-let sanValentin2 = new SanValentin (17, "Brownie LOVE", 21200, "../assets/imagenes/productos/brownie-love.jpg", 1, "brownie", "sanvalentin");
-let sanValentin3 = new SanValentin (18, "Cupcakes de Amor", 700, "../assets/imagenes/productos/cupcakes-love.jpg", 1, "bocaditos y postres", "sanvalentin");
-let sanValentin4 = new SanValentin (19, "Torta Corazón", 15500, "../assets/imagenes/productos/torta-corazon.jpg", 1, "torta", "sanvalentin");
+let sanValentin1 = new SanValentin (16, "Box Love", 15000 ,"./assets/imagenes/productos/box-sanvalentin.jpg", 1, "bocaditos y postres", "sanvalentin" );
+let sanValentin2 = new SanValentin (17, "Brownie LOVE", 21200, "./assets/imagenes/productos/brownie-love.jpg", 1, "brownie", "sanvalentin");
+let sanValentin3 = new SanValentin (18, "Cupcakes de Amor", 700, "./assets/imagenes/productos/cupcakes-love.jpg", 1, "bocaditos y postres", "sanvalentin");
+let sanValentin4 = new SanValentin (19, "Torta Corazón", 15500, "./assets/imagenes/productos/torta-corazon.jpg", 1, "torta", "sanvalentin");
 
 //convierto en array los objeros creados
 let  productoSanValentin = [sanValentin1, sanValentin2, sanValentin3, sanValentin4];
